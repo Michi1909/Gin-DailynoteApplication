9ef6dff5b9462ae5cfa7ac36ef489f7c03d5f40f
Michi1909 <michaela.kaiser1993@outlook.de>
2023-03-12 04:17:34 +0100

    dailynote Java Spring Boot Framework

---
+++ JavaFolder/dailynote/src/main/java/com/dailynote/dailynote/DailynoteApplication.java
@@ -0,0 +1,13 @@
+package com.dailynote.dailynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailynoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailynoteApplication.class, args);
+	}
+
+}
---
+++ JavaFolder/dailynote/src/test/java/com/dailynote/dailynote/DailynoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailynote.dailynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailynoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
